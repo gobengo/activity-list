0c91ddba0820e25567c51a0cda9b102a94c22452
Benjamin Goering <bengoering@gmail.com>
2014-06-26 16:22:04 -0700

    show more works

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
+'use strict';
+
 module.exports = ActivityList;
 
 var ActivityElement = require('activity-element');
-var PassThrough = require('readable-stream/passthrough');
-var Transform = require('readable-stream/transform');
 var extend = require('util-extend');
-var inherits = require('inherits');
-var isArray = require('is-array');
 var through = require('through2');
 var map = require('through2-map');
 var filter = require('through2-filter');
@@ -16,27 +14,31 @@ var filter = require('through2-filter');
  */
 function ActivityList(el) {
     this.el = el || document.createElement('li');
-    this.pageSize = 2;
+    this.pageSize = 10;
     // activities that have been read
     this.activities = [];
     // future elements (streamed)
-    this._future = through.obj({ highWaterMark: 0, lowWaterMark: 0 });
+    this._future = through.obj({ highWaterMark: 0, lowWaterMark: 0 })
+        .on('data', function (newEl) {
+            // prepend
+            el.insertBefore(newEl, el.firstChild);
+        });
     // more elements (usually from the past)
     this._more = through.obj({ highWaterMark: 0, lowWaterMark: 0 });
-
-    this._future.on('data', function (newEl) {
-        // prepend
-        el.insertBefore(newEl, el.firstChild);
-    });
     this.showMore(this.pageSize);
 }
 
+/**
+ * Render an Activity object into an HTMLElement
+ */
 ActivityList.prototype.renderActivity = function (activity) {
+    // if already an HTMLElement, let through as is
     if (activity && activity.nodeType === 1) {
-        // if already an HTMLElement, let through as is
+        console.error('HTMLElement passed to renderActivity');
         return activity;
     }
-    return ActivityElement(activity);
+    var el = ActivityElement(activity);
+    return el;
 };
 
 /**
@@ -58,30 +60,61 @@ ActivityList.prototype.stream = function (stream) {
  */
 ActivityList.prototype.streamMore = function (archive) {
     archive
-        .pipe(map.obj(this.renderActivity))
-        .pipe(this._more);
+        .pipe(this._createRenderer())
+        .pipe(this._more, { end: false });
+};
+
+ActivityList.prototype._createRenderer = function () {
+    var opts = { highWaterMark: 0, lowWaterMark: 0 };
+    var renderer = map.obj(opts, this.renderActivity);
+    return renderer;
 };
 
 ActivityList.prototype.showMore = function(amount) {
     amount = amount || this.pageSize;
     var listEl = this.el;
-    this._more
-        .pipe(limit(amount))
-        .on('data', function (el) {
-            listEl.appendChild(el);
-        });
+    var more = this._more;
+    readN(more, amount, function (el) {
+        listEl.appendChild(el);
+    });
+    // TODO: refactor this. It's disgusting but
+    // piping to limit(amount) wasn't working so good
+    function readN(stream, n, cbEach) {
+        var thing;
+        while (n && (thing = more.read())) {
+            n = n - 1;
+            cbEach(thing);
+        }
+        if (n) {
+            more.once('readable', function () {
+                readN(stream, n, cbEach);
+            });
+        }
+    }
 };
 
 /**
  * Create a Duplex that only passes through N written items, then ends
  */
 function limit(remaining) {
-    return through.obj(function (chunk, encoding, done) {
+    var opts = {highWaterMark: 0, lowWaterMark: 0};
+    return through.obj(opts, function (chunk, encoding, done) {
+        console.log('chunk through limit. remaining=',remaining);
         this.push(chunk);
         remaining = remaining - 1;
         if (! remaining) {
             return this.push(null);
         }
         done();
+    });
+}
+
+function logStream(msg) {
+    var opts = {highWaterMark: 0, lowWaterMark: 0};
+    return through.obj(opts, function (chunk, e, done) {
+        console.log('logStream', msg);
+        setTimeout(function () {
+            done(null, chunk);
+        }, 50);
     });
 }
